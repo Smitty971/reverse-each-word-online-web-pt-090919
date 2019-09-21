6f78321c446985d841632bc726b267b59944f579
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-21 21:43:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,10 @@ def reverse_each_word(string)
  string = string.split(" ")
  reversed_string = []
  
- string.each do |char|
-   
- 
- return reversed_string.join(" ")
-end
-
-string.collect do |char|
+   string.collect do |char|
   char.reverse 
 end
+ 
+ 
+end
+
